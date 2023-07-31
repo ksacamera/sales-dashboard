952fd7b9c0274a3fc5f91ac853742a954f61b8c1
Kevin Anderson <ksacamera@gmail.com>
2023-07-31 13:54:01 -0700

    first commit

---
+++ sales-dashboard/src/App.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function BasicGrid() {
+  return (
+    <>
+    <h1 id="pizza">Pizza Face Pizza Company</h1>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item><h2>DASHBOARD</h2>
+            <h4>welcome to your dashboard</h4></Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item><h2>Charts</h2>
+            <h4>revenue generated</h4></Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>Sales Obtained</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>New Clients</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>Traffic Received</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>Revenue Generated</Item>
+        </Grid>
+      </Grid>
+    </Box>
+    </>
+  );
+}
